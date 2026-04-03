61662e03bbcd13c0b48d3e6e2317e84ccd16ae2e
Vinayak <vinayakatke7@gmail.com>
2026-04-04 05:08:50 +0530

    almost done

---
+++ lab4/sudoku_solver.py
@@ -0,0 +1,132 @@
+import pycosat as psat
+import sys, os, copy
+input_file = sys.argv[1]
+
+with open(input_file, 'r') as file:
+    content = file.read()
+    data = content.split("\n")
+
+data.pop()
+
+games = []
+
+for i in data:
+    row = 0
+    col = 0
+    n = 9
+    m = 9
+
+    mat = [[0 for _ in range(m)] for _ in range(n)]
+    for j in i:
+        if row == 9:
+            col += 1
+        row %= 9
+        mat[row][col] = j
+        row += 1
+    
+    games.append(mat)
+
+def cell(x, y, z):
+    return 100*x + 10*y + z + 1
+
+def decell(x):
+    return x//100, (x//10 % 10, x % 10)
+
+base_clause = []
+atleast_one = []
+atmax_one = []
+uniqrow = []
+uniqcol = []
+smaller_block = []
+    
+for rw in range(0, 9):
+    for cl in range(0, 9):
+        l = []
+        for n1 in range(0, 9):
+            l.append(cell(n1, rw, cl))
+        atleast_one.append(l)
+    
+for rw in range(0, 9):
+    for cl in range(0, 9):
+        for n1 in range(0, 9):
+            for m1 in range(n1+1, 9):
+                x1 = [-1*cell(n1, rw, cl), -1*cell(m1, rw, cl)]
+                atmax_one.append(x1)
+
+for rw in range(0, 9):
+    for n1 in range(0, 9):
+        l = []
+        for cl in range(0, 9):
+            l.append(cell(n1, rw, cl))
+        uniqrow.append(l)
+
+for cl in range(0, 9):
+    for n1 in range(0, 9):
+        l = []
+        for rw in range(0, 9):
+            l.append(cell(n1, rw, cl))
+        uniqcol.append(l)
+    
+for n1 in range(0, 3):
+    for m1 in range(0, 3):
+        for num in range(0, 9):
+            l = []
+            for j in range(n1*3, n1*3 + 3):
+                for k in range(m1*3, m1*3 + 3):
+                    l.append(cell(num, j, k))
+            smaller_block.append(l)
+
+for group in atleast_one:
+    base_clause.append(group)
+
+for group in atmax_one:
+    base_clause.append(group)
+
+for group in uniqrow:
+    base_clause.append(group)
+
+for group in uniqcol:
+    base_clause.append(group)
+
+for group in smaller_block:
+    base_clause.append(group)
+
+for group in smaller_block:
+    for n1 in range(len(group)):
+        for m1 in range(n1 + 1, len(group)):
+            base_clause.append([-group[n1], -group[m1]])
+
+for i in games:
+    #main loop in which each clause will be made.
+    clauses = copy.deepcopy(base_clause)
+    
+    init = []
+
+    for n1 in range(0, 9):
+        for m1 in range(0, 9):
+            if i[n1][m1] != '.':
+                i1 = int(i[n1][m1]) - 1
+                init.append([cell(i1, n1, m1)])
+                
+    clauses.extend(init)
+    
+    sol = psat.solve(clauses)
+    
+    out = []
+
+    for i in sol:
+        if i > 0:
+            num, (x1, y1) = decell(i)
+            print(x1, y1)
+            out.append(num)
+    print(out)
+
+output_file = os.path.join(os.path.dirname(os.path.abspath(input_file)), "output.txt")
+
+result = 0
+
+try:
+    with open(output_file, "w") as f:
+        f.write(result)
+except:
+    print(result)
